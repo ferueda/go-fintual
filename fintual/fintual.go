1adebadddb5fe927aa84032c3b59f110979bfc4b
ferueda <fe.rueda@outlook.com>
2021-10-21 20:09:15 -0300

    Create fintual.go

---
+++ fintual/fintual.go
@@ -0,0 +1,10 @@
+// Package fintual provides utilties for interfacing
+// with the Fintual's API.
+package fintual
+
+// Version is the version of this library.
+const Version = "1.0.0"
+
+const (
+	defaultBaseURL = "https://fintual.cl/api/"
+)
